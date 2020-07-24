c6899b173b3294b348521527bc54f4cfbd363f6e
agusTrih <ant3dong@gmail.com>
2020-07-24 22:00:53 +0700

    add fetch login register in action

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -7,3 +7,47 @@ const getUserLogin = (data) => {
         data,
     };
 };
+
+const login = (formData, history) => async (dispatch) => {
+    const url = `${process.env.REACT_APP_BACKEND_ENDPOINT}/api/users/login`;
+
+    const response = await fetch(url);
+    const result = await response.json();
+
+    const checkUser = result.filter((user) => {
+        return formData.email === user.email;
+    });
+    if (checkUser.length > 0) {
+        if (checkUser[0].password !== formData.password) {
+            alert("email atau password anda masih salah tuh");
+        } else {
+            dispatch(getUserLogin(checkUser[0]));
+            alert(`Welcome back ${checker[0].fullname}`);
+            history.push("/home");
+        }
+    } else {
+        alert(`email anda tidak ada di sini bos`);
+    }
+};
+
+// register
+const registerUser = (formData, history) => async (dispatch) => {
+    const url = "https://5e33d48de0161c00140ac4fe.mockapi.io/users";
+    const options = {
+        method: "POST",
+        body: JSON.stringify(formData),
+        headers: {
+            "Content-type": "application/json",
+        },
+    };
+
+    const response = await fetch(url, options);
+    await response.json();
+
+    if (response.status === 201) {
+        alert("Terima kasih sudah mendaftar");
+        history.push("/");
+    }
+};
+
+export { login, GET_USER_LOGIN, LOGIN, registerUser };
